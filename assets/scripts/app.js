3f7112524ab9bb841558eae5f16bb78f9ded5416
Yaroslav Senkiv <slavko.senkiv@gmail.com>
2023-11-11 07:10:05 +0100

    learn new atuff about arrays

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,7 @@
 
+/////////////////////////////////////////////////
+// Import
+/////////////////////////////////////////////////
 /*
 import * as util from './util';
 console.log(util.apiKey);
@@ -6,22 +9,25 @@ console.log(util.apiKey2); */
 import util, { apiKey, apiKey2 as apiKey3} from "./util";
 console.log(apiKey3);
 
+/////////////////////////////////////////////////
+// Variables
+/////////////////////////////////////////////////
 const x = 123;
 // x = 321; cannot be reassigned
-
 let y = 123;
 y = 321; // can be reassigned
-
 console.log(x);
 console.log(y);
 
+/////////////////////////////////////////////////
+// Functions
+/////////////////////////////////////////////////
 function greetUser(username, greeting = 'Hello ') {
   console.log(greeting, username);
 }
-
 greetUser('Slav');
 greetUser('Zoe', 'Whats app ');
-
+///////////////
 let fun = (name, age) => {
   console.log(name + age);
 }
@@ -42,3 +48,17 @@ console.log(fun4(21));
 ///////////
 let fun5 = age =>  ({old: age});
 console.log(fun5);
+/////////////////////////////////////////////////
+// Arrays
+/////////////////////////////////////////////////
+const hobbies = ['a', 'b', 'c'];
+//console.log(hobbies.findIndex('a')); wrong way to do it
+// foloving variant is ok but longer more readable version
+/* const index = hobbies.findIndex((item)=>{
+  return item === 'b'; // must be === not just =
+});
+console.log(index); */
+// folowing variant is shorter but less readable version
+console.log(hobbies.findIndex(item => item === 'b'));
+console.log(hobbies.map(item => item + '!'));
+
